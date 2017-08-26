60c40666a67f2e833e5ce04e36d1751afaa78526
johanduran <johan.durancerdas@gmail.com>
2017-08-26 06:54:30 +0000

    previo a crear modelos de bd

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 'use strict'
 
 var mongoose = require("mongoose");
+var app = require("./app");
+var port = process.env.PORT || 2977;
+
 
 mongoose.connect("mongodb://localhost:27017/musifidb", (err,res)=>{
     if(err){
         throw err;
     }else{
         console.log("correct");
+        app.listen(port,function() {
+            console.log("servidor escuchando");
+        });
     }
 });
